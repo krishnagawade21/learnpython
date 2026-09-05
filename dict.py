2254baa12d16669e62e3c9c27971f6ea97c059a4
krishnagawade21 <krishnagawade2402@gmail.com>
2026-09-05 12:50:58 +0530

    adding dict files

---
+++ dict.py
@@ -0,0 +1,17 @@
+# key : value pair
+dict = {
+    "name" : "krishna",
+    "cgpa" : 9.8,
+    "subject" : "python",
+}
+
+
+print(type(dict))
+
+print(dict)
+
+#methods 
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.get("name"))
