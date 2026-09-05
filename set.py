a517e34d7519fe6b49d27caffe5f5f34293eddde
krishnagawade21 <krishnagawade2402@gmail.com>
2026-09-05 13:14:02 +0530

    add  set file

---
+++ set.py
@@ -0,0 +1,11 @@
+collection = {1,2.2,6,9,6,3,7,4,4,9,2}
+print(collection)
+print(type(collection))
+
+print(len(collection))
+# method in set
+
+collection.add(10)
+collection.remove(1)
+collection.pop()
+print(collection)
