ae4d4cf318d26e91fa29b1592ccabfb12e79ffce
krishnagawade21 <krishnagawade2402@gmail.com>
2026-09-21 21:09:15 +0530

    adding tuple file

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# Tuple program in Python
+
+student = ("Krishna", 20, "Computer Engineering", 85.5)
+
+print("Student Name:", student[0])
+print("Age:", student[1])
+print("Branch:", student[2])
+print("Percentage:", student[3])
+
+print("\nComplete Tuple:")
+print(student)
+
+print("\nNumber of elements:", len(student))
